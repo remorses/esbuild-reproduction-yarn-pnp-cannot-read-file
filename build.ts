41e4cd1a94a4090209a06f2f8532fbff783dee83
remorses <beats.by.morse@gmail.com>
2020-12-01 18:54:46 +0100

    more options

--- build.ts
+++ build.ts
@@ -5,6 +5,8 @@ build({
     bundle: true,
     outdir: 'bundle',
     format: 'esm',
+    splitting: true,
+    metafile: 'meta.json',
     target: 'es2017',
     entryPoints: ['./src/index.ts'],
     plugins: [PnpResolver()],
